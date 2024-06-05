fc771a5b8c8e2ea161ac8b466393e91468586b2d
Tangaente <niki.haack@gmail.com>
2024-06-05 09:33:09 +0200

    Wir haben jetzt msg

--- src/Main.elm
+++ src/Main.elm
@@ -1,61 +1,52 @@
 module Main exposing (main)
 
 import Browser
-import Html
-    exposing
-        ( Attribute
-        , Html
-        , a
-        , button
-        , div
-        , fieldset
-        , img
-        , input
-        , label
-        , node
-        , table
-        , td
-        , text
-        , th
-        , tr
-        )
+import Browser.Events exposing (onResize)
+import Html exposing (Html, div, text, img)
 import Html.Attributes exposing (checked, coords, for, href, id, name, shape, src, style, title, type_, usemap)
-import Html.Events exposing (onClick, onInput)
-import List exposing (append, length, map, map2, range)
+import Json.Decode as Decode
+
+
+-- MODEL
 
 type alias Model =
-    { count : Int }
+    { width : Int
+    , height : Int
+    }
 
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( Model 0 0, Cmd.none )
 
-initialModel : Model
-initialModel =
-    { count = 0 }
-
+-- UPDATE
 
 type Msg
-    = Increment
-    | Decrement
+    = WindowResized Int Int
 
-
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Increment ->
-            { model | count = model.count + 1 }
+        WindowResized width height ->
+            ( { model | width = width, height = height }, Cmd.none )
 
-        Decrement ->
-            { model | count = model.count - 1 }
+-- SUBSCRIPTIONS
 
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    onResize WindowResized
 
-view : Model -> Html Msg  
+-- VIEW
+
+view : Model -> Html Msg
 view model =
-        div[][img [src  "Tavern.jpeg", style "width" "1000px", style "height" "1000px"] []]
+    div[][img [src  "Tavern.jpeg", style "width" (String.fromInt model.width), style "height" (String.fromInt model.height)] []]
 
+-- MAIN
 
-main : Program () Model Msg
 main =
-    Browser.sandbox
-        { init = initialModel
-        , view = view
+    Browser.element
+        { init = init
         , update = update
+        , subscriptions = subscriptions
+        , view = view
         }
